25a7d59d5a0591cf73481f795650a6d2adbab38f
Lotte <lmanenschijn@sogyo.nl>
2023-10-10 18:07:47 +0200

    einde van dag

---
+++ api/src/main/java/stratego/api/repository/BordstateRepository.java
@@ -0,0 +1,8 @@
+// package stratego.api.repository;
+
+// import stratego.api.models.Bordstate;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface BordstateRepository extends JpaRepository<Bordstate, Long> {
+//     // You can define custom query methods here if needed
+// }
