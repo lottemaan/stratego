9777571ad88685e6ab8cc29e78f1ac4909512316
Lotte <lmanenschijn@sogyo.nl>
2023-10-12 16:23:11 +0200

    unit tests toegevoegd en code coverage minimum 90%

--- domain/src/test/java/stratego/domain/FirstTest.java
+++ domain/src/test/java/stratego/domain/FirstTest.java
@@ -1,7 +1,7 @@
 package stratego.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +9,48 @@ public class FirstTest {
 
     @Test
     public void ThisTestShouldPass() {
-        boolean failingTest = true;
-        assertEquals(true, failingTest); 
+        boolean passingTest = true;
+        assertEquals(true, passingTest); 
     }
+
+
+    @Test
+    public void TestIfStrategogameCanBeCreated() {
+        StrategoGame strategoGame = new StrategoGame("Jantje", "Jip");
+        assertNotNull(strategoGame);
+    }
+
+    @Test
+    public void TestIfPlayerOneIsCalledJantje() {
+        StrategoGame strategoGame = new StrategoGame("Jantje", "Jip");
+        assertEquals(strategoGame.getNameOfPlayerOne(), "Jantje");        
+    }
+
+    @Test
+    public void TestIfPlayerTwoIsCalledJip() {
+        StrategoGame strategoGame = new StrategoGame("Jantje", "Jip");
+        assertEquals(strategoGame.getNameOfPlayerTwo(), "Jip");        
+    }
+
+    @Test
+    public void TestIfGameHasNotEnded() {
+        StrategoGame strategoGame = new StrategoGame("Jantje", "Jip");
+        boolean isEndOfGame = strategoGame.isEndOfGame();
+        assertEquals(isEndOfGame, false);
+    }
+
+
+    @Test
+    public void TestIfStukCanBeCreated() {
+        Stuk stuk = new Stuk();
+        assertNotNull(stuk);
+    }
+
+    @Test
+    public void TestIfVakCanBeCreated() {
+    Vak vak = new Vak();
+    assertNotNull(vak);
+    }
+
+
 }
