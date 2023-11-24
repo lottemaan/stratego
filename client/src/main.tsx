7dc73adced2ff35f627e51bd695ce9dc909dbdb7
Lotte Manenschijn <lmanenschijn@sogyo.nl>
2023-11-24 11:17:55 +0100

    configuration provider toegevoegd

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 import { StrategoGameProvider } from './contexts/StrategoGameContext.tsx'
+import { ConfigurationProvider } from './contexts/ConfigurationContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ConfigurationProvider>
     <StrategoGameProvider>
-      <RouterProvider router={router} />
+      
+        <RouterProvider router={router} />
+   
     </StrategoGameProvider>
+    </ConfigurationProvider>
   </React.StrictMode>,
-)
+);
